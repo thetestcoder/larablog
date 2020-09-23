770040fc5ecb3f70402d3b7b46a8943974659765
The Test Coder <thetestcoder@gmail.com>
2020-09-23 08:39:34 +0530

    Chapter 18 Code

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,152 @@
+<div class="col-md-4">
+    <div class="col-md-12 nopadding">
+        <div class="blog1-sidebar-box">
+            <div class="image-holder"><img src="http://placehold.it/360x340" alt="" class="img-responsive"/></div>
+            <div class="text-box-inner">
+                <h5 class="uppercase dosis">About me</h5>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue. </p>
+                <br/>
+                <div class="clearfix"></div>
+                <ul class="blog1-social-icons">
+                    <li><a href="https://twitter.com/codelayers"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://www.facebook.com/codelayers"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--end author info-->
+
+    <div class="col-md-12 nopadding">
+        <div class="blog1-sidebar-box">
+            <div class="text-box-inner">
+                <h5 class="uppercase dosis">Search</h5>
+                <div class="clearfix"></div>
+                <input class="blog1-sidebar-serch_input" type="search" placeholder="Email Address">
+                <input name="" value="Submit" class="blog1-sidebar-serch-submit" type="submit">
+            </div>
+        </div>
+    </div>
+    <!--end sidebar box-->
+
+    <div class="col-md-12 nopadding">
+        <div class="blog1-sidebar-box">
+            <div class="text-box-inner">
+                <h5 class="uppercase dosis">Flickr Feed</h5>
+                <div class="clearfix"></div>
+                <ul id="basicuse" class="thumbs">
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--end sidebar box-->
+
+    <div class="col-md-12 nopadding">
+        <div class="blog1-sidebar-box">
+            <div class="text-box-inner">
+                <h5 class="uppercase dosis">Categories</h5>
+                <div class="clearfix"></div>
+                <ul class="category-links">
+                    <li><a href="#">Business</a></li>
+                    <li><a class="active" href="#">Education</a></li>
+                    <li><a href="#">Medical</a></li>
+                    <li><a href="#">Travel</a></li>
+                    <li><a href="#">Photogreaphy</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--end sidebar box-->
+
+    <div class="col-md-12 nopadding">
+        <div class="blog1-sidebar-box">
+            <div class="text-box-inner">
+                <h5 class="uppercase dosis">Latest Posts</h5>
+                <div class="clearfix"></div>
+                <div class="blog1-sidebar-posts">
+                    <div class="image-left"><img src="http://placehold.it/80x80" alt=""/></div>
+                    <div class="text-box-right">
+                        <h6 class="less-mar3 uppercase dosis nopadding"><a href="#">Clean And Modern</a></h6>
+                        <p>Lorem ipsum dolor sit</p>
+                        <div class="post-info"> <span>By John Doe</span><span>May 19</span> </div>
+                    </div>
+                </div>
+                <!--end item-->
+
+                <div class="blog1-sidebar-posts active">
+                    <div class="image-left"><img src="http://placehold.it/80x80" alt=""/></div>
+                    <div class="text-box-right">
+                        <h6 class="less-mar3 uppercase dosis nopadding"><a href="#">Clean And Modern</a></h6>
+                        <p>Lorem ipsum dolor sit</p>
+                        <div class="post-info"> <span>By John Doe</span><span>May 19</span> </div>
+                    </div>
+                </div>
+                <!--end item-->
+
+                <div class="blog1-sidebar-posts">
+                    <div class="image-left"><img src="http://placehold.it/80x80" alt=""/></div>
+                    <div class="text-box-right">
+                        <h6 class="less-mar3 uppercase dosis nopadding"><a href="#">Clean And Modern</a></h6>
+                        <p>Lorem ipsum dolor sit</p>
+                        <div class="post-info"> <span>By John Doe</span><span>May 19</span> </div>
+                    </div>
+                </div>
+                <!--end item-->
+
+            </div>
+        </div>
+    </div>
+    <!--end sidebar box-->
+
+    <div class="col-md-12 nopadding">
+        <div class="blog1-sidebar-box">
+            <div class="text-box-inner">
+                <h5 class="uppercase dosis">Tags</h5>
+                <div class="clearfix"></div>
+                <ul class="tags">
+                    <li><a href="#">Animation</a></li>
+                    <li><a href="#">Art</a></li>
+                    <li><a href="#">UI Design</a></li>
+                    <li><a href="#">Photography</a></li>
+                    <li><a class="active" href="#">Design</a></li>
+                    <li><a href="#">Art</a></li>
+                    <li><a href="#">Responsive</a></li>
+                    <li><a href="#">Develop</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--end sidebar box-->
+
+    <div class="col-md-12 nopadding">
+        <div class="blog1-sidebar-box">
+            <div class="text-box-inner">
+                <h5 class="uppercase dosis">Newsletter</h5>
+                <div class="clearfix"></div>
+                <input class="blog1-sidebar-serch_input" type="search" placeholder="Email Address">
+                <input name="" value="Submit" class="blog1-sidebar-serch-submit" type="submit">
+            </div>
+        </div>
+    </div>
+    <!--end sidebar box-->
+
+    <div class="col-md-12 nopadding">
+        <div class="blog1-sidebar-box">
+            <div class="text-box-inner">
+                <h5 class="uppercase dosis">Featured Works</h5>
+                <div class="clearfix"></div>
+                <ul class="sidebar-works">
+                    <li><a href="#"><img src="http://placehold.it/90x90" alt=""/></a></li>
+                    <li><a href="#"><img src="http://placehold.it/90x90" alt=""/></a></li>
+                    <li class="last"><a><img src="http://placehold.it/90x90" alt=""/></a></li>
+                    <li><a href="#"><img src="http://placehold.it/90x90" alt=""/></a></li>
+                    <li><a href="#"><img src="http://placehold.it/90x90" alt=""/></a></li>
+                    <li class="last"><a href="#"><img src="http://placehold.it/90x90" alt=""/></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--end sidebar box-->
+
+</div>
