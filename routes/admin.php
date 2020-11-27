591fc13f17fbb2b4bc41abaa81cff5171eb9ac57
The Test Coder <thetestcoder@gmail.com>
2020-11-27 09:09:19 +0530

    Chapter 37 Code

--- routes/admin.php
+++ routes/admin.php
@@ -1,38 +1,53 @@
 <?php
 
 Route::group([
-    'middleware'=>['auth'],
-    'prefix'=>'backpanel'
-], function (){
+    'middleware' => ['auth'],
+    'prefix' => 'backpanel'
+], function () {
     Route::view('/', 'backpanel.dashboard.index')->name('backpanel.dashboard');
 
+    Route::group(['middleware'=>'role:admin'], function () {
+        //user routes
+        Route::resource('/user', 'User\UserController');
+        //role routes
+        Route::get('/role/{role}/assign-permission', 'User\RoleController@assignPermissionView')
+            ->name('role.assign.permission');
 
-//user routes
-    Route::resource('/user', 'User\UserController');
+        Route::post('/role/{role}/assign-permission', 'User\RoleController@assignPermission')
+            ->name('role.store.permission');
 
-
-//role routes
-    Route::get('/role/{role}/assign-permission', 'User\RoleController@assignPermissionView')
-        ->name('role.assign.permission');
-
-    Route::post('/role/{role}/assign-permission', 'User\RoleController@assignPermission')
-        ->name('role.store.permission');
-
-    Route::resource('/role', 'User\RoleController');
+        Route::resource('/role', 'User\RoleController');
 
 
-//permission routes
-    Route::resource('/permission', 'User\PermissionController');
+        //permission routes
+        Route::resource('/permission', 'User\PermissionController');
+    });
 
-//category Route
-    Route::get('/category/trashed', 'User\CategoryController@trashedCategory')
-        ->name('category.trash');
-    Route::post('/category/{category}/restore', 'User\CategoryController@restoreCategory')
-        ->name('category.restore');
-    Route::delete('/category/{category}/force-delete', 'User\CategoryController@forceDeleteCategory')
-        ->name('category.force.delete');
-    Route::resource('/category', 'User\CategoryController');
+    Route::group(['middleware' => "role:admin|editor"], function (){
+        //category Route
+        Route::get('/category/trashed', 'User\CategoryController@trashedCategory')
+            ->name('category.trash');
+        Route::post('/category/{category}/restore', 'User\CategoryController@restoreCategory')
+            ->name('category.restore');
+        Route::delete('/category/{category}/force-delete', 'User\CategoryController@forceDeleteCategory')
+            ->name('category.force.delete');
+        Route::resource('/category', 'User\CategoryController');
 
+        // tags
+        Route::get('/tag/trashed', 'TagController@trashedTag')
+            ->name('tag.trash');
+
+
+        Route::post('/tag/{tag}/restore', 'TagController@restoreTag')
+            ->name('tag.restore');
+
+
+        Route::delete('/tag/{tag}/force-delete', 'TagController@forceDeleteTag')
+            ->name('tag.force.delete');
+
+
+        Route::resource('/tag', "TagController");
+    });
 
 
 //post route
@@ -44,20 +59,4 @@ Route::group([
     Route::delete('/post/{post}/force-delete', 'User\PostController@forceDeletePost')
         ->name('post.force.delete');
     Route::resource('/post', 'User\PostController');
-
-
-// tags
-    Route::get('/tag/trashed', 'TagController@trashedTag')
-        ->name('tag.trash');
-
-
-    Route::post('/tag/{tag}/restore', 'TagController@restoreTag')
-        ->name('tag.restore');
-
-
-    Route::delete('/tag/{tag}/force-delete', 'TagController@forceDeleteTag')
-        ->name('tag.force.delete');
-
-
-    Route::resource('/tag', "TagController");
 });
